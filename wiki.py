809da107da1f27cbf0578a2d98d0506979a6275b
Sukhdeep Singh <105273235+sukhioo7@users.noreply.github.com>
2023-09-16 13:23:02 +0530

    Update wiki.py

--- wiki.py
+++ wiki.py
@@ -1,35 +1,36 @@
 import tkinter as tk
-import wikipedia 
+import wikipedia
 
-def result():
-    query = user_search.get()
-    print(query)
+def wiki_pedia():
+    user_search = search_input.get()
     try:
-        result = wikipedia.summary(query,sentences = 2)
-        result_label.config(text=result,font=('Bell MT',17),wraplength=600,relief='ridge',padx=20,pady=20,fg='black')
+        result = wikipedia.summary(user_search,sentences=3)
+        result_label.config(text=result)
     except:
-        result_label.config(text='No Data Found',fg='#B22222',font=('Bell MT',17),wraplength=600,relief='ridge',padx=20,pady=20)
-        
+        result_label.config(text='No Data Found',font=('Harlow Solid Italic',25)
+                            ,pady=20,fg='#CE0601')
+
 
 window = tk.Tk()
-window.title('Wikipedia')
-window.geometry('650x700')
-window.resizable(0.0,0.0)
+window.geometry('800x600')
+window.title('WikipediA')
+window.config(bg='#FCF5E5')
 
-heading = tk.Label(text='WikipediA',font=('Bell MT',80),fg='#2E8B57')
-heading.pack(pady=15)
+title = tk.Label(text='WikipediA',font=('Harlow Solid Italic',75),fg='#CE0601',bg='#FCF5E5')
+title.pack()
 
-search = tk.Label(text='Search Here',font=('Bell MT',28))
-search.pack()
-
-user_search = tk.Entry(font=('Bell MT',18),width=35)
-user_search.pack(pady=8)
+sub_title = tk.Label(text='Search Here...',font=('Harlow Solid Italic',25),fg='#B7B9AF',bg='#FCF5E5')
+sub_title.pack()
 
 
-search_btn = tk.Button(text='Search',font=('Bell MT',18,'bold'),padx=170,bg='#2E8B57',fg='white',relief='ridge',command=result)
-search_btn.pack()
+search_input = tk.Entry(font=('Bahnschrift Light Condensed',25),width=30,bg='#B7B9AF')
+search_input.pack()
 
-result_label = tk.Label(text='')
-result_label.pack(pady=20)
+btn = tk.Button(text='Search',font=('Harlow Solid Italic',23)
+                ,relief='groove',bg='#4e5454',width=22,fg='white',command=wiki_pedia)
+btn.pack(pady=5)
+
+result_label = tk.Label(text='',font=('Bahnschrift Light Condensed',15),bg='#FCF5E5',wraplength=700)
+result_label.pack()
 
 window.mainloop()
