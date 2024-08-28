6756f07c94d785e49703c0f80ff36c05a3188615
Sukhdeep Singh <105273235+sukhioo7@users.noreply.github.com>
2024-08-28 13:52:16 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,5 +1,11 @@
 import tkinter as tk 
 
+def print_hello():
+        num1 = eval(user_input1.get())
+        num2 = eval(user_input2.get())
+        result = num1 + num2
+        output.config(text=f'Result : {result}')
+
 window = tk.Tk()
 window.title('MY title')
 window.geometry('700x600')
@@ -23,7 +29,14 @@ label2.pack(pady=5)
 user_input2 = tk.Entry(font=('HP Simplified Hans',20),highlightbackground='#0E46A3',highlightthickness=1)
 user_input2.pack()
 
-add_btn = tk.Button(text='SUM',font=('Bauhaus 93',20),width=20,bg='#0E46A3',fg='white',relief='solid')
+add_btn = tk.Button(text='SUM',font=('Bauhaus 93',20),width=20,bg='#0E46A3',
+                    fg='white',relief='solid',command=print_hello)
 add_btn.pack(pady=5)
 
+output = tk.Label(font=('HP Simplified Hans',20),bg='#E1F7F5',
+                  fg='#1E0342')
+output.pack(pady=15)
+
 window.mainloop()
+
+
