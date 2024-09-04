d795b63f2c5206f6da93b691496dd1cdbb46227b
Sukhdeep Singh <105273235+sukhioo7@users.noreply.github.com>
2024-09-04 12:49:49 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,42 +1,127 @@
-import tkinter as tk 
-
-def print_hello():
-        num1 = eval(user_input1.get())
-        num2 = eval(user_input2.get())
-        result = num1 + num2
-        output.config(text=f'Result : {result}')
-
-window = tk.Tk()
-window.title('MY title')
-window.geometry('700x600')
-window.config(bg='#E1F7F5')
+# ----------------------------------------YOUTUBE DOWNLOADER-------------------------------------------------------------
 
 
-heading = tk.Label(text='Calculator',font=('Bauhaus 93',52),bg='#E1F7F5',fg='#0E46A3')
+import tkinter as tk
+from tkinter import messagebox,filedialog,ttk
+from pytubefix import YouTube
+from pytubefix.cli import on_progress
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO
+
+def download_video():
+    link = video_link.get()
+    if not link:
+        messagebox.showerror("Error", "Please enter a valid YouTube Link")
+        return
+    
+    
+    folder = filedialog.askdirectory()
+    if not folder:
+        return
+
+
+    try:
+        youtube = YouTube(link, on_progress_callback = on_progress)
+    
+        ys = youtube.streams.get_highest_resolution()
+        ys.download(output_path=folder)
+        messagebox.showinfo('Success',f'Downloaded {youtube.title} successfully')
+    except Exception as e:
+        messagebox.showerror('Error',f'Failed to download video.\n{e}')
+        
+    
+def download_audio():
+    link = video_link.get()
+    if not link:
+        messagebox.showerror("Error", "Please enter a valid YouTube Link")
+        return
+    
+    
+    folder = filedialog.askdirectory()
+    if not folder:
+       return
+    
+    try:
+        youtube = YouTube(link, on_progress_callback = on_progress)
+    
+        ys = youtube.streams.filter(only_audio=True).first()  
+        ys.download(output_path=folder)
+        messagebox.showinfo('Success',f'Downloaded audio of {youtube.title} successfully')
+    except Exception as e:
+        messagebox.showerror("Error", f"Failed to download audio.\n{e}")
+    
+
+
+def fetch_video_info():
+    link = video_link.get()
+    if not link:
+        messagebox.showwarning("Input Error", "Please enter a YouTube URL")
+        return
+    
+    try:
+        yt = YouTube(link)
+        title = yt.title
+        author = yt.author
+        length = f"{yt.length // 60} minutes {yt.length % 60} seconds"
+        views = f"{yt.views:,}"  # Add commas to the view count
+        
+        info_text = f"Title: {title}\nAuthor: {author}\nLength: {length}\nViews: {views}\n"
+
+        info_window = tk.Toplevel(window)
+        info_window.title("Video Information")
+        info_window.geometry("400x200")
+        
+        info_label = tk.Label(info_window, text=info_text,font = ("Arial",6,'bold'),bg="#f0f0f0",justify=tk.LEFT)
+        info_label.pack(pady=10)
+
+        thumbnail_url = yt.thumbnail_url
+        response = requests.get(thumbnail_url)
+        img_data = BytesIO(response.content)
+        img = Image.open(img_data)
+        img = img.resize((160, 80), Image.Resampling.LANCZOS)
+        
+        img_tk = ImageTk.PhotoImage(img)
+        
+        thumbnail_label = tk.Label(info_window, image=img_tk)
+        thumbnail_label.image = img_tk  # Keep a reference to prevent garbage collection
+        thumbnail_label.pack(pady=10)
+
+    except Exception as e:
+        messagebox.showerror("Error", f"Failed to fetch video information: {e}")
+
+
+window =  tk.Tk()
+window.title("Youtube Audio and Video Downloader")
+window.geometry("700x600")
+window.config(bg = '#FFF6EA')
+
+heading = tk.Label(text = 'Free YouTube Video Downloader',font = ('Georgla Pro',30,'bold','underline'),bg='white',fg='#E85C0D')
 heading.pack(pady=20)
 
-label1 = tk.Label(text='Enter Number 1: ',font=('HP Simplified Hans',20),bg='#E1F7F5',
-                  fg='#1E0342')
-label1.pack(pady=5)
+heading1 = tk.Label(text = ' Enter YouTube Video URL :',font = ('Arial',20,'bold'),bg='white',fg='black')
+heading1.pack(pady=20)
 
-user_input1 = tk.Entry(font=('HP Simplified Hans',20),highlightbackground='#0E46A3',highlightthickness=1)
-user_input1.pack()
+video_link = tk.Entry(font = ('Arial',20),width=50,bg = 'light gray',highlightbackground ='#F7EED3',highlightthickness = 1)
+video_link.pack(pady=10)
 
-label2 = tk.Label(text='Enter Number 2: ',font=('HP Simplified Hans',20),bg='#E1F7F5',
-                  fg='#1E0342')
-label2.pack(pady=5)
+info_button = tk.Button(text="Video Info",font=('Arial',15,'bold'),width=50,fg='white',bg='#E85C0D', command=fetch_video_info,relief='ridge')
+info_button.pack(pady=30)
 
-user_input2 = tk.Entry(font=('HP Simplified Hans',20),highlightbackground='#0E46A3',highlightthickness=1)
-user_input2.pack()
 
-add_btn = tk.Button(text='SUM',font=('Bauhaus 93',20),width=20,bg='#0E46A3',
-                    fg='white',relief='solid',command=print_hello)
-add_btn.pack(pady=5)
+quality_label = tk.Label(window , text="Select Video Quality :", font=("Arial", 14,'bold'),bg='blue')
+quality_label.pack(anchor='s',pady=20)
 
-output = tk.Label(font=('HP Simplified Hans',20),bg='#E1F7F5',
-                  fg='#1E0342')
-output.pack(pady=15)
+quality_choice = tk.StringVar()
+quality_dropdown = ttk.Combobox(textvariable=quality_choice, font=("Arial", 14))
+quality_dropdown['values'] = ('1080p', '720p', '480p', '360p', '240p', '144p')
+quality_dropdown.current(1)  # Default to 720p
+quality_dropdown.pack(padx=10, pady=1)
+
+video_btn = tk.Button(text = 'Download Video',font = ('Arial',15),width=15,fg='white',bg='green',command=download_video)
+video_btn.pack(pady=15)
+
+audio_btn = tk.Button(text = 'Download Audio',font = ('Arial',15),width=15,fg='white',bg='green',command=download_audio)
+audio_btn.pack(pady=10)
 
 window.mainloop()
-
-
