906bf48e7d209bbf0b14ad2ea04c4c98828907ff
Sukhdeep Singh <105273235+sukhioo7@users.noreply.github.com>
2024-09-04 13:55:25 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,7 +1,15 @@
 import tkinter as tk
+from pytubefix import YouTube
+from pytubefix.cli import on_progress
 
 def fetch_video_info():
     link = link_entry.get()
+    yt = YouTube(link, on_progress_callback = on_progress)
+
+    title = yt.title
+    author = yt.author
+    length = f"{yt.length // 60} minutes {yt.length % 60} seconds"
+    views = f"{yt.views:,}"  # Add commas to the view count
 
     info_window = tk.Toplevel(window)
     info_window.title('Video Information')
@@ -12,20 +20,14 @@ def fetch_video_info():
                     font=('Harlow Solid Italic', 55), bg='white', fg='#C63C51')
     heading.grid(row=1, column=1,padx=63,pady=30,columnspan=2)
 
-    heading = tk.Label(info_window,text='Arithmetic & Assignment Operators | Python Practice Playlist | Python tutorial for beginners in 2024',
+    heading = tk.Label(info_window,text=title,
                     font=('Helvetica', 23,'bold'),wraplength=950,justify='left' ,bg='white', fg='#173B45')
     heading.grid(row=2, column=1,padx=10,columnspan=2)
 
-
-
-    # author = yt.author
-    # length = f"{yt.length // 60} minutes {yt.length % 60} seconds"
-    # views = f"{yt.views:,}"  # Add commas to the view count
-
     gernal_info = f'''
-Author : Punjabi CodeX
-Length : 8 minutes 45 seconds
-Views : 3434
+Author : {author}
+Length : {length}
+Views : {views}
 '''
     
     gernal_heading = tk.Label(info_window,text=gernal_info,font=('Helvetica',14),bg='white',
@@ -36,9 +38,16 @@ Views : 3434
                     )
     keyword_area.grid(row=3, column=2,padx=10)
 
+    keyword_area.delete(0.0,tk.END)
+    keyword_area.insert(0.0, yt.keywords)
+
     description_area = tk.Text(info_window,width=88,height=14,font=('Helvetica',14),relief='solid',
                     )
     description_area.grid(row=4, column=1,padx=10,columnspan=2)
+
+    description_area.delete(0.0,tk.END)
+    description_area.insert(0.0, yt.description)
+
 
 
 window = tk.Tk()
@@ -58,7 +67,7 @@ link_entry = tk.Entry(font=('Helvetica', 18), width=40, bg='lightgray', relief='
 link_entry.grid(row=3, column=1,pady=20)
 
 get_info_btn = tk.Button(text='Get info',font=('Harlow Solid Italic',20),
-                         bg='#522258',fg='white',width=20,relief='solid')
+                         bg='#522258',fg='white',width=20,relief='solid',command=fetch_video_info)
 get_info_btn.grid(row=4, column=1)
 
 download_btn = tk.Button(text='Download Video',font=('Harlow Solid Italic',20),
