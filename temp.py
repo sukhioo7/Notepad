4a251cd942d5f57acbbb40e00c6133ef6c9781e0
Sukhdeep Singh <105273235+sukhioo7@users.noreply.github.com>
2024-09-04 13:44:58 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,5 +1,45 @@
 import tkinter as tk
 
+def fetch_video_info():
+    link = link_entry.get()
+
+    info_window = tk.Toplevel(window)
+    info_window.title('Video Information')
+    info_window.geometry('1000x700')
+    info_window.config(bg='white')
+
+    heading = tk.Label(info_window,text='Video Information',
+                    font=('Harlow Solid Italic', 55), bg='white', fg='#C63C51')
+    heading.grid(row=1, column=1,padx=63,pady=30,columnspan=2)
+
+    heading = tk.Label(info_window,text='Arithmetic & Assignment Operators | Python Practice Playlist | Python tutorial for beginners in 2024',
+                    font=('Helvetica', 23,'bold'),wraplength=950,justify='left' ,bg='white', fg='#173B45')
+    heading.grid(row=2, column=1,padx=10,columnspan=2)
+
+
+
+    # author = yt.author
+    # length = f"{yt.length // 60} minutes {yt.length % 60} seconds"
+    # views = f"{yt.views:,}"  # Add commas to the view count
+
+    gernal_info = f'''
+Author : Punjabi CodeX
+Length : 8 minutes 45 seconds
+Views : 3434
+'''
+    
+    gernal_heading = tk.Label(info_window,text=gernal_info,font=('Helvetica',14),bg='white',
+                              justify='left',anchor='e')
+    gernal_heading.grid(row=3, column=1,padx=10)
+
+    keyword_area = tk.Text(info_window,width=60,height=4,font=('Helvetica',14),relief='solid',
+                    )
+    keyword_area.grid(row=3, column=2,padx=10)
+
+    description_area = tk.Text(info_window,width=88,height=14,font=('Helvetica',14),relief='solid',
+                    )
+    description_area.grid(row=4, column=1,padx=10,columnspan=2)
+
 
 window = tk.Tk()
 window.title('YoyTube Downloader')
